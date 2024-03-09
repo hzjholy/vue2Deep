4224ec5d89a50d78585ee3bab29e3503adf93ba9
hzjholy <41406542+hzjholy@users.noreply.github.com>
2024-03-09 21:35:17 +0800

    使用rollup搭建环境

---
+++ README.md
@@ -0,0 +1,13 @@
+# 深入学习vue2
+
+
+1.使用rollup搭建开发环境
+ cnpm i rollup rollup-plugin-babel @babel/core @babel/preset-env --save-dev
+
+ rollup -cw 打包文件并监听变化
+
+
+ 为什么vue2只能支持ie9以及以上
+ Object.defineProperty【es5】不支持低版本
+
+ proxy是es6的  也没有替代方案
---
+++ rollup.config.mjs
@@ -0,0 +1,18 @@
+// rollup默认可以导出一个对象作为打包的配置文件
+import babel from "rollup-plugin-babel";
+
+export default {
+  input: "./src/index.js", // 入口
+  output: {
+    file: "./dist/vue.js", // 出口
+    // new Vue【全局新增】
+    name: "Vue", // global.vue
+    format: "umd", // esm es6模块 commonjs模块（node） iife(自执行函数) umd(统一模块规范,兼容commonjs amd)
+    sourcemap: true, // 希望可以调试源代码
+  },
+  plugins: [
+    babel({
+      exclude: "node_modules/**", // 排除node_modules所有文件 **代表任意文件夹
+    }),
+  ],
+};
